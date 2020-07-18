875b9605b0063e73aa627877490da71ddc286b6f
miltaf <mahboob.iltaf@gmail.com>
2020-07-18 16:17:19 -0400

    new changes added

--- src/class01/task1.java
+++ src/class01/task1.java
@@ -2,4 +2,10 @@ package class01;
 
 public class task1 {
 
+	public static void main (String[] args) {
+		System.out.println("Hello");
+		System.out.println("Hello");
+		System.out.println("Hello");
+		
+	}
 }
